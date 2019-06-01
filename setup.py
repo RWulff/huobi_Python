8c077a4a20279db5f4b4418701b77a6978532284
Raffael Wulff <raffisaccount@hotmail.de>
2019-06-01 23:45:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(
-    name="huobi-client",
+    name="huobi",
     version="0.1",
     packages=['huobi', 'huobi.impl', 'huobi.impl.utils', 'huobi.exception', 'huobi.model'],
     install_requires=['requests', 'apscheduler', 'websocket', 'websocket-client', 'urllib3']
